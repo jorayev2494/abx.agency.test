54e0f2caec225746f995ff61a8795085d8e07ffa
Yakub Jorayev <jorayev2494@gmail.com>
2024-09-07 01:08:13 +0400

    Change swagger server address

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 use OpenApi\Attributes as OA;
 
 #[
-    OA\Info(version: "1.0.0", description: "Azb agency test", title: "Documentation of Azb agency test"),
-    OA\Server(url: 'http://localhost:8011', description: "Local server"),
-    OA\Server(url: 'http://31.131.17.136:8011', description: "Staging server"),
-    OA\SecurityScheme(securityScheme: 'bearerAuth', type: "http", name: "Authorization", in: "header", scheme: "bearer"),
+    OA\Info(version: '1.0.0', description: 'Azb agency test', title: 'Documentation of Azb agency test'),
+    OA\Server(url: 'http://31.131.17.136:8011', description: 'Staging server'),
+    OA\Server(url: 'http://localhost:8011', description: 'Local server'),
+    OA\SecurityScheme(securityScheme: 'bearerAuth', type: 'http', name: 'Authorization', in: 'header', scheme: 'bearer'),
 ]
 abstract class Controller
 {
